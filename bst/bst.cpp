621eab43f2fded4bd9878cdc9e6ae4415e23db51
Keshav Malhotra <malhotrakeshav10@gmail.com>
2026-04-17 13:04:35 -0400

    added bst

--- bst/bst.cpp
+++ bst/bst.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue> //only used for bfs
 using namespace std;
 template <typename T>
 class TreeNode
@@ -84,7 +85,7 @@ public:
         }
         size++;
     }
-    void inOrder(TreeNode<T> *root)
+    void inOrder(TreeNode<T> *root) // 1+2 --> '1' = left,  '+' = print, '2' = right
     {
         if (root == nullptr)
             return;
@@ -96,7 +97,7 @@ public:
     {
         inOrder(root);
     }
-    void postOrder(TreeNode<T> *root)
+    void postOrder(TreeNode<T> *root) // 12+ --> '1' = left, '2' = right, '+' = print
     {
         if (root == nullptr)
             return;
@@ -108,14 +109,51 @@ public:
     {
         postOrder(root);
     }
+    void preOrder(TreeNode<T> *root) // +12 --> '+' = print,  '1' = left, '2' = right
+    {
+        if (root == nullptr)
+            return;
+        cout << root->val << " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+    void preOrder()
+    {
+        preOrder(root);
+    }
+    void bfs(TreeNode<T> *root)
+    {
+        if (root == nullptr) // indicates BST empty
+            return;
+        queue<TreeNode<T> *> q; // create a queue q that holds pointers to TreeNodes
+        q.push(root);
+        while (!q.empty())
+        {                                     // while our queue is not empty
+            TreeNode<T> *current = q.front(); // look at top at queue
+            q.pop();                          // pop it
+            cout << current->val << ' ';      // print the value
+            if (current->left != nullptr)     // if a left child exists, then insert into queue
+                q.push(current->left);
+            if (current->right != nullptr)
+            { // if right child exists, then insert into queue
+                q.push(current->right);
+            }
+        }
+    }
+    void bfs()
+    {
+        bfs(root);
+    }
 };
 
 int main()
 {
-    BST<int> myBST(1);
+    BST<int> myBST(10);
+    myBST.add(5);
+    myBST.add(60);
     myBST.add(3);
-    myBST.add(4);
-    myBST.add(2);
-    myBST.inOrder();
-    myBST.postOrder();
+    myBST.add(7);
+    myBST.add(30);
+    myBST.add(70);
+    myBST.bfs();
 }
